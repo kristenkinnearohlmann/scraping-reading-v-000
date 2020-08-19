2fb0ab9de108b4abf3b04ed3d639508f5992f428
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-19 16:57:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-#puts doc.css(".headline-26OIBN").text.strip
+p doc.css(".headline-26OIBN").text.strip
 courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course|

05db18bef1bab66b9ab2836be8d455289e7c1f06
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-19 16:45:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,4 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 #puts doc.css(".headline-26OIBN").text.strip
-puts doc.css(".title-oE5vT4")
+puts doc.css(".text-4GLMvr .title-oE5vT4")

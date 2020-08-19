87239466bd982ccd56f838fa1fdda87c0099ca9e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-19 17:00:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,8 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
 # puts doc.css(".headline-26OIBN").text.strip
-p doc.css(".headline-26OIBN")
-# p doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
+
+p doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 # courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 #
 # courses.each do |course|
